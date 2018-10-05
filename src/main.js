514c616d6ee13ff39810f483c12b08777083f0c2
Ryan <ryan.dsilva.98@gmail.com>
2018-10-05 17:52:30 +0530

    Added Login and Signup

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 import 'babel-polyfill'
 import Vue from 'vue'
+import VueSession from 'vue-session'
+import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import App from './App'
 import router from './router'
+import store from './store'
 
+Vue.use(VueSession, {
+  persist: true
+})
+axios.defaults.baseURL = 'http://localhost:3000'
 Vue.config.productionTip = false
-
 Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Login from '@/components/user/Login'
+import Register from '@/components/user/Register'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
