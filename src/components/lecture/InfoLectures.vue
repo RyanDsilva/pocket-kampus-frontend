43ece7160eef8b853149bf486772fba6f0bf41f6
Anushka Paradkar <anupara15@gmail.com>
2018-10-05 18:15:20 +0530

    info about lectures

---
+++ src/components/lecture/InfoLectures.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="page-heading">
+        <h1>MEP</h1>
+        <h2>Time</h2>
+        <p>8:45am-9:45am</p>
+        <h3>Notes</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sequi explicabo nobis iste rerum maiores ipsum esse facilis atque doloribus exercitationem nihil, non at laudantium tenetur optio dicta amet mollitia. Illum, alias fugit culpa nemo veritatis ex possimus iusto placeat quae est sit neque dolores repellendus iure impedit, atque qui. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis sequi explicabo nobis iste rerum maiores ipsum esse facilis atque doloribus exercitationem nihil, non at laudantium tenetur optio dicta amet mollitia. Illum, alias fugit culpa nemo veritatis ex possimus iusto placeat quae est sit neque dolores repellendus iure impedit, atque qui.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'infolectures'
+}
+</script>
+
+<style scoped lang="scss">
+#page-heading {
+  text-align: center;
+  color: #808080;
+
+  h1{
+    font-weight:700;
+    color: #3949ab;
+    font-size: 2em
+  }
+h2{
+   color:#3949ab;
+   }
+h3{
+   color:#3949ab;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import InfoLectures from '@/components/lecture/InfoLectures'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/lectures/info',
+      name: 'infolectures',
+      component: InfoLectures
     }
   ]
 })
